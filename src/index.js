07142f10354f8168291c235e0e80c74fa8e9a1ee
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-24 22:18:46 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('input'),
+    list: document.querySelector('ul'),
+}
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event){
+    event.preventDefault();
+
+    console.log(event.currentTarget);
+
+    const searchCountry = event.currentTarget.elements.query.value;
+    const url = 'https://restcountries.com/v3.1/name/' + searchCountry;
+    //const options = {} ?
+
+    const countryFetch = fetch(url)
+    .then( r => {return r.json()})
+    .then(console.log)
+    .catch( error => console.log(error));
+}
