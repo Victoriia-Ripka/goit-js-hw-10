6e3d43b2b46d15e48c00bcfad6db873cec2dfedb
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-25 15:49:33 +0300

    without hbs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import './css/styles.css';
-import countryCardTpi from './templates/country-card.hbs';
-//do the markup
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    input: document.querySelector('input'),
-    list: document.querySelector('ul'),
-}
+  input: document.querySelector('input'),
+  list: document.querySelector('ul'),
+};
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function onInput(event){
-    event.preventDefault();
+function onInput(event) {
+  event.preventDefault();
 
-    const searchCountry = event.target.value;
-    const url = `https://restcountries.com/v3.1/name/${searchCountry}`;
-    //const options = {} ?
+  const searchCountry = event.target.value;
+  const url = `https://restcountries.com/v3.1/name/${searchCountry}`;
+  //const options = {} ?
 
-    const countryFetch = fetch(url)
-    .then( r => {return r.json()})
+  const countryFetch = fetch(url)
+    .then(r => {
+      return r.json();
+    })
     .then(console.log)
-    .catch( error => console.log(error));
+    .catch(error => console.log(error));
 }
 
-function renderCountryCard(country){
-    const markup = countryCardTpi(country);
-    refs.list.innerHTML = markup;
-}
+// function renderCountryCard(country) {
+//   const markup = countryCardTpi(country);
+//   refs.list.innerHTML = markup;
+// }
