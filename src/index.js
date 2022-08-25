0bb4e2fea92b2fc49bacc025d5652794a365e5c6
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-08-25 14:24:48 +0300

    спроба handlebars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './css/styles.css';
+import countryCardTpi from './templates/country-card.hbs';
+//do the markup
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
@@ -13,14 +15,17 @@ refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(event){
     event.preventDefault();
 
-    console.log(event.currentTarget);
-
-    const searchCountry = event.currentTarget.elements.query.value;
-    const url = 'https://restcountries.com/v3.1/name/' + searchCountry;
+    const searchCountry = event.target.value;
+    const url = `https://restcountries.com/v3.1/name/${searchCountry}`;
     //const options = {} ?
 
     const countryFetch = fetch(url)
     .then( r => {return r.json()})
     .then(console.log)
     .catch( error => console.log(error));
+}
+
+function renderCountryCard(country){
+    const markup = countryCardTpi(country);
+    refs.list.innerHTML = markup;
 }
